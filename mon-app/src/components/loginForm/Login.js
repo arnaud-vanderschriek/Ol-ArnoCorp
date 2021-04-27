8b13b985969a809312fa99a6688bc1909ac179fc
d <arnaud.vanderschrieck@gmail.com>
2021-04-27 15:26:00 +0200

    add a background for my input

--- mon-app/src/components/loginForm/Login.js
+++ mon-app/src/components/loginForm/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './styles/styles.css';
 
 function Login() {
-  
   return (
-    <form>
+    <form className='loginForm'>
       <label>mon premier input</label>
       <input type='text'></input>
     </form>
