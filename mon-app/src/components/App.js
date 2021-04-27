f247a40a1a92721983b218a8653a725ab26c79f9
d <arnaud.vanderschrieck@gmail.com>
2021-04-27 15:19:19 +0200

    add a form input

--- mon-app/src/components/App.js
+++ mon-app/src/components/App.js
@@ -1,10 +1,10 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import Login from './loginForm/Login';
 
 function App() {
   return (
     <div className="App">
-     <p>Heyyeyee</p>
+      <Login />
     </div>
   );
 }
---
+++ mon-app/src/components/loginForm/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Login() {
+  
+  return (
+    <form>
+      <label>mon premier input</label>
+      <input type='text'></input>
+    </form>
+  )
+}
+
+export default Login;
